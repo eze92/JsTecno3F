d02846333b1007f91c462886698244d686ab898a
unknown <ezequiel.ledesma026@gmail.com>
2025-04-18 11:02:04 -0300

    se agrega ejercicio for

---
+++ clese04/app.js
@@ -0,0 +1,9 @@
+/*
+Escribir un programa que cuente los números del 1 al 10 e imprima cada número en la consola. 
+*/
+
+let numero = parseInt(prompt("Ingrese un numero para mostrar la cantidad: "));
+
+for ( let i = 1 ; i <= numero ; i++){
+    console.log( "La cantidad de numeros es: ", i)
+}
