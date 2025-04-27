d48c2bda0367b3e68f8d01dcd14ebce48c3b65f1
unknown <ezequiel.ledesma026@gmail.com>
2025-04-27 12:07:47 -0300

    se agrega ejercicio 1 clase 5

---
+++ clase05/app.js
@@ -0,0 +1,8 @@
+/*Ejercicio 1
+Dado el arreglo de frutas que contienen "manzana", "banana", "pera", "naranja", recorrerlo con un bucle e imprimir por consola el índice y el valor de cada elemento.*/
+
+const frutas = ["manzana", "banana", "pera", "naranja"];
+
+for ( let i = 0; i <= frutas.length - 1; i++){
+    console.log("El indice es:" , i , "y el valor es:" , frutas[i] )
+}
