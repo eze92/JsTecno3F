b330db494762fb42caefc17a06c517a86c9bbbe8
unknown <ezequiel.ledesma026@gmail.com>
2025-04-27 12:24:56 -0300

    se agregan metodos al arreglo

--- clase05/app.js
+++ clase05/app.js
@@ -6,3 +6,26 @@ const frutas = ["manzana", "banana", "pera", "naranja"];
 for ( let i = 0; i <= frutas.length - 1; i++){
     console.log("El indice es:" , i , "y el valor es:" , frutas[i] )
 }
+
+
+/*Ejercicio 2
+Con el arreglo anterior, realizar las siguientes 
+
+1. Agregar "kiwi" al final.*/
+frutas.push("Kiwi");
+console.log( frutas )
+
+/*2. Agregar "sandía" al inicio.*/
+
+frutas.unshift("sandia");
+console.log( frutas )
+
+/*3. Quitar el primer elemento.*/
+
+frutas.shift();
+console.log( frutas )
+
+/*4. Quitar el último elemento. Imprime el arreglo final.*/
+
+frutas.pop();
+console.log( frutas )
